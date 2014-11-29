c350288cbb7201e3529a0fd34f75d5f03e93be4b
Anton ALLIOT <anton.alliot@live.se>
2014-11-29 22:44:27 +0100

    norm

--- head.h
+++ head.h
@@ -6,7 +6,7 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/25 17:46:38 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/29 20:02:02 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/29 21:16:41 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,72 @@
 
 # define HEAD_H
 
+# define WIN_SIZE_X 2400
+# define WIN_SIZE_Y 1200
+
 # include "libft/includes/get_next_line.h"
 # include <math.h>
 # include <mlx.h>
 
-typedef struct			s_env
-{
-	void				*mlx;
-	void				*win;
-}						t_env;
+typedef unsigned char	t_byte;
 
-typedef struct			s_pos
+typedef struct	s_env
 {
-	int					x;
-	int 				y;
-}						t_pos;
+	void		*mlx;
+	void		*win;
+}				t_env;
 
-typedef struct			s_pt_mouse
+typedef struct	s_pos
 {
-	int					x;
-	int					y;
-}						t_pt_mouse;
+	int			x;
+	int			y;
+}				t_pos;
 
-typedef struct			s_3dpos
+typedef struct	s_pt_mouse
 {
-	int					x;
-	int					y;
-	int					z;
-}						t_3dpos;
+	int			x;
+	int			y;
+}				t_pt_mouse;
 
-typedef struct 			s_all
+typedef struct	s_3dpos
 {
-	t_3dpos				**tab;
-	t_pos				max;
-}						t_all;
+	int			x;
+	int			y;
+	int			z;
+}				t_3dpos;
 
-typedef struct			s_color
+typedef struct	s_all
 {
-	unsigned char		r;
-	unsigned char		g;
-	unsigned char		b;
-}						t_color;
+	t_3dpos		**tab;
+	t_pos		max;
+}				t_all;
 
-int						key_hook(int keycode, t_env *e);
-int						ft_color_to_int(t_color color);
-void					ft_put_pixel(t_env env, t_pos point, t_color color);
-void					ft_line_put(t_env env, t_pos p1, t_pos p2, t_color color);
-void					ft_3d_line_put(t_env env, t_3dpos dp1, t_3dpos dp2, t_color color);
-void					ft_put_3d_tab(t_env env, t_3dpos **tab, t_color color, t_pos max);
-t_pos					ft_3d_to_2d(t_3dpos dp1);
-t_all					ft_read_map(int fd, char c);
-t_3dpos					**ft_fill_tab(t_list *lst, t_3dpos pt);
+typedef struct	s_color
+{
+	t_byte		r;
+	t_byte		g;
+	t_byte		b;
+}				t_color;
+
+typedef struct	s_line
+{
+	t_pos		p1;
+	t_pos		p2;
+	t_color		clr;
+	t_pos		inc;
+	int			dx;
+	int			dy;
+}				t_line;
+
+int				key_hook(int keycode, t_env *e);
+int				ft_color_to_int(t_color c);
+void			ft_put_pixel(t_env env, t_pos point, t_color c);
+void			ft_line_put(t_env env, t_pos p1, t_pos p2, t_color clr);
+void			ft_line_put_1(t_env env, t_line line);
+void			ft_line_put_2(t_env env, t_line line);
+void			ft_put_3d_tab(t_env env, t_3dpos **tab, t_color c, t_pos max);
+t_pos			ft_3d_to_2d(t_3dpos dp1);
+t_all			ft_read_map(int fd, char c);
+t_3dpos			**ft_fill_tab(t_list *lst, t_3dpos pt);
 
 #endif
--- main.c
+++ main.c
@@ -6,123 +6,123 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/29 12:17:29 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/29 20:16:07 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/11/29 22:27:25 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 #include <stdio.h>
 
-t_pos	ft_3d_to_2d(t_3dpos dp1)
+t_pos			ft_3d_to_2d(t_3dpos dp1)
 {
-	// float cst1 = 1;
-	// float cst2 = 1;
-	t_pos p1;
+	t_pos		p1;
 
 	p1.x = dp1.x - dp1.y;
 	p1.y = dp1.z + (dp1.x / 2) + (dp1.y / 2);
-
 	return (p1);
 }
 
-int		ft_color_to_int(t_color color)
+int				ft_color_to_int(t_color clr)
 {
-	int c;
+	int			c;
 
-	c = color.r;
+	c = clr.r;
 	c = c << 8;
-	c += color.g;
+	c += clr.g;
 	c = c << 8;
-	c += color.b;
+	c += clr.b;
 	return (c);
 }
 
-void	ft_put_pixel(t_env env, t_pos point, t_color color)
+void			ft_put_pixel(t_env env, t_pos point, t_color clr)
 {
-	mlx_pixel_put(env.mlx, env.win, point.x, point.y, ft_color_to_int(color));
+	mlx_pixel_put(env.mlx, env.win, point.x, point.y, ft_color_to_int(clr));
 }
 
-void	ft_line_put(t_env env, t_pos p1, t_pos p2, t_color color)
+void			ft_line_put(t_env env, t_pos p1, t_pos p2, t_color clr)
 {
-	int		i;
-	int		xinc;
-	int		yinc;
-	int		cumul;
+	t_line		line;
 
-	p2.x -= p1.x;
-	p2.y -= p1.y;
-	xinc = (p2.x > 0) ? 1 : -1;
-	yinc = (p2.y > 0) ? 1 : -1;
-	p2.x = abs(p2.x);
-	p2.y = abs(p2.y);
-  	ft_put_pixel(env, p1, color);
-  	i = 1;
-  	if (p2.x > p2.y)
-  	{
-    	cumul = p2.x / 2 ;
-    	while (i <= p2.x)
-    	{
-			p1.x += xinc ;
-			cumul += p2.y ;
-			if ( cumul >= p2.x )
-			{
-				cumul -= p2.x ;
-				p1.y += yinc;
-			}
-    	  	ft_put_pixel(env, p1, color); 
- 			i++;
- 		}
- 	}
-    else
-    {
-    	cumul = p2.y / 2;
-    	while (i <= p2.y)
-    	{
-			p1.y += yinc;
-			cumul += p2.x;
-			if ( cumul >= p2.y )
-			{
-				cumul -= p2.y;
-				p1.x += xinc;
-			}
-				ft_put_pixel(env, p1, color);
-			i++;
-  		}
+	line.inc.x = ((p2.x - p1.x) > 0) ? 1 : -1;
+	line.inc.y = ((p2.y - p1.y) > 0) ? 1 : -1;
+	line.dx = abs(p2.x - p1.x);
+	line.dy = abs(p2.y - p1.y);
+	line.p1 = p1;
+	line.p2 = p2;
+	line.clr = clr;
+	if (line.dx > line.dy)
+		ft_line_put_1(env, line);
+	else
+		ft_line_put_2(env, line);
+}
+
+void			ft_line_put_1(t_env env, t_line line)
+{
+	int			i;
+	int			cumul;
+
+	i = 1;
+	cumul = line.dx / 2;
+	while (i++ <= line.dx)
+	{
+		line.p1.x += line.inc.x;
+		cumul += line.dy;
+		if (cumul >= line.dx)
+		{
+			cumul -= line.dx;
+			line.p1.y += line.inc.y;
+		}
+		ft_put_pixel(env, line.p1, line.clr);
 	}
 }
 
-void	ft_3d_line_put(t_env env, t_3dpos dp1, t_3dpos dp2, t_color color)
+void			ft_line_put_2(t_env env, t_line line)
 {
-	t_pos p1;
-	t_pos p2;
+	int			i;
+	int			cumul;
+
+	i = 1;
+	cumul = line.dy / 2;
+	while (i++ <= line.dy)
+	{
+		line.p1.y += line.inc.y;
+		cumul += line.dx;
+		if (cumul >= line.dy)
+		{
+			cumul -= line.dy;
+			line.p1.x += line.inc.x;
+		}
+		ft_put_pixel(env, line.p1, line.clr);
+	}
+}
+
+void			ft_3d_line(t_env env, t_3dpos dp1, t_3dpos dp2, t_color clr)
+{
+	t_pos		p1;
+	t_pos		p2;
 
 	dp1.x = dp1.x * 20 + 800;
 	dp1.y = dp1.y * 20 + 400;
-	//dp1.z *= 102
 	dp2.x = dp2.x * 20 + 800;
 	dp2.y = dp2.y * 20 + 400;
-	//dp2.z *= 10;
 	p1 = ft_3d_to_2d(dp1);
 	p2 = ft_3d_to_2d(dp2);
-	ft_line_put(env, p1, p2, color);
+	ft_line_put(env, p1, p2, clr);
 }
 
-void	ft_put_3d_tab(t_env env, t_3dpos **tab, t_color color, t_pos max)
+void			ft_put_3d_tab(t_env env, t_3dpos **tab, t_color clr, t_pos max)
 {
-	int x;
-	int y;
-
-
+	int			x;
+	int			y;
 
 	y = 0;
 	while (y + 1 < max.y)
 	{
 		x = 0;
-
 		while (x + 1 < max.x)
 		{
-			ft_3d_line_put(env, tab[y][x], tab[y][x + 1], color);
-			ft_3d_line_put(env, tab[y][x], tab[y + 1][x], color);	
+			ft_3d_line(env, tab[y][x], tab[y][x + 1], clr);
+			ft_3d_line(env, tab[y][x], tab[y + 1][x], clr);
 			x++;
 		}
 		y++;
@@ -130,21 +130,15 @@ void	ft_put_3d_tab(t_env env, t_3dpos **tab, t_color color, t_pos max)
 	y = 0;
 	x = 0;
 	while (y + 1 < max.y)
-	{
-		ft_3d_line_put(env, tab[y][max.x - 1], tab[y + 1][max.x - 1], color);
-		y++;
-	}
+		ft_3d_line(env, tab[y++][max.x - 1], tab[y + 1][max.x - 1], clr);
 	while (x + 1 < max.x)
-	{
-		ft_3d_line_put(env, tab[max.y - 1][x], tab[max.y - 1][x + 1], color);
-		x++;
-	}
+		ft_3d_line(env, tab[max.y - 1][x++], tab[max.y - 1][x + 1], clr);
 }
 
-t_3dpos				**ft_fill_tab(t_list *lst, t_3dpos pt)
+t_3dpos			**ft_fill_tab(t_list *lst, t_3dpos pt)
 {
-	t_3dpos			**tab;
-	int				i;
+	t_3dpos		**tab;
+	int			i;
 
 	i = 0;
 	tab = (t_3dpos**)malloc(sizeof(t_3dpos*) * pt.y);
@@ -153,7 +147,6 @@ t_3dpos				**ft_fill_tab(t_list *lst, t_3dpos pt)
 	while (lst->next)
 	{
 		pt = *((t_3dpos*)lst->content);
-//		printf("%d %d = %d\n", pt.x, pt.y, pt.z);
 		pt.z = -pt.z;
 		tab[pt.y][pt.x] = pt;
 		lst = lst->next;
@@ -161,14 +154,13 @@ t_3dpos				**ft_fill_tab(t_list *lst, t_3dpos pt)
 	return (tab);
 }
 
-t_all				ft_read_map(int fd, char c)
+t_all			ft_read_map(int fd, char c)
 {
-	char			*tmp;
-	char			**tabx;
-	t_3dpos			pt;
-	t_3dpos			**tab;
-	t_list			*lst;
-	t_all			all;
+	char		*tmp;
+	char		**tabx;
+	t_3dpos		pt;
+	t_list		*lst;
+	t_all		all;
 
 	lst = NULL;
 	pt.y = 0;
@@ -176,38 +168,37 @@ t_all				ft_read_map(int fd, char c)
 	{
 		pt.x = 0;
 		tabx = ft_strsplit(tmp, c);
+		free(tmp);
 		while (*tabx)
 		{
-			pt.z = ft_atoi(*tabx);
+			pt.z = ft_atoi(*tabx++);
 			ft_lstadd(&lst, ft_lstnew(&pt, sizeof(t_3dpos)));
-			tabx++;
 			pt.x++;
 		}
 		pt.y++;
 	}
-	tab = ft_fill_tab(lst, pt);
-	all.tab = tab;
+	all.tab = ft_fill_tab(lst, pt);
 	all.max.x = pt.x;
 	all.max.y = pt.y;
 	return (all);
 }
 
-int					key_hook(int keycode, t_env *e)
+int				key_hook(int keycode, t_env *e)
 {
 	if (keycode == 65307)
 		exit (0);
 	return (0);
 }
 
-int					main(void)
+int				main(void)
 {
-	int				fd;
-	t_env			env;
-	t_color			blue = {0, 0, 0xFF};
-	t_all			all;
+	int			fd;
+	t_env		env;
+	t_color		blue = {0, 0, 0xFF};
+	t_all		all;
 
 	env.mlx = mlx_init();
-	env.win = mlx_new_window(env.mlx, 2400, 1200, "F_D_F");
+	env.win = mlx_new_window(env.mlx, WIN_SIZE_X, WIN_SIZE_Y, "F_D_F");
 	mlx_key_hook(env.win, key_hook, &env);
 	fd = open("42.fdf", O_RDONLY);
 	all = ft_read_map(fd, ' ');
